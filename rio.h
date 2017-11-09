849b72f3b0d7cc99b32a39e3bbe141eebbe2330c
whoamishuaige <2634692095@qq.com>
2017-11-09 21:40:17 +0800

    rio.h 的升级版本,加入了rio_read函数等一系列操作

--- rio.h
+++ rio.h
@@ -1,53 +1,154 @@
-/*
- *rio.h 用于健壮的io。robust io.用于处理read，write种各种原因产生的不足值 short num
- * 提供两类函数，一类是无缓冲io输入输出函数，直接在存储器和文件之间传送数据,没有应用级别缓冲
- * 一类是带缓冲的函数，是线程安全的
- * 开始rio.h
- * */
-#ifndef __RIO_H__
-#define __RIO_H_
-/*ssize_t 类型为有符号的int，原因是他可能返回-1，size_t是无符号的int，原因是他们传入的时候肯定为正数，所以size_t的范围多ssize_t一倍左右*/
-ssize_t rio_readn(int fd,void *usrbuf,size_t n);//不带缓冲的读函数
-ssize_t rio_writen(int fd,void *usrbuf,size_t n);//不带缓冲的写函数
+#include <stdio.h> 
+#include <string.h> 
+#include <errno.h> 
+#include <sys/types.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+ 
+#define MAXLINE 1024 
+ 
+/*unbuffer input/output function*/ 
+ 
+ssize_t rio_readn(int fd,void *usrbuf,size_t n) 
+{ 
+    size_t nleft = n; 
+    ssize_t nread; 
+    char *bufp = usrbuf; 
+ 
+    while(nleft > 0){ 
+        if((nread = read(fd,bufp,nleft)) < 0){ 
+            if(errno == EINTR){/*interrupted by sig handler return*/ 
+                nread = 0; 
+            }else{ 
+                return -1;/*error*/ 
+            } 
+        }else if(nread == 0){ 
+            break;  /*EOF*/ 
+        }else{/*read content*/ 
+            nleft -= nread; 
+            bufp += nread; 
+        } 
+    } 
+    return (n - nleft); 
+} 
+ 
+ssize_t rio_writen(int fd,void *usrbuf,size_t n) 
+{ 
+    size_t nleft = n; 
+    ssize_t nwritten; 
+    char *bufp = usrbuf; 
+ 
+    while(nwritten = write(fd,bufp,nleft) <= 0){ 
+        if(errno == EINTR){ 
+            nwritten = 0; 
+        }else{ 
+            return -1; 
+        } 
+        nleft -= nwritten; 
+        bufp += nwritten; 
+    } 
+    return n; 
+} 
+/******************************************************************************/ 
+#define RIO_BUFSIZE 8192 
+typedef struct{ 
+    int rio_fd; /*To operate the file descriptor*/ 
+    int rio_cnt;/*unread bytes in internal buf*/ 
+    char *rio_bufptr;/*next unread byte int internal buf*/ 
+    char rio_buf[RIO_BUFSIZE];/*internal buf*/ 
+}rio_t; 
+void rio_readinitb(rio_t *rp,int fd) 
+{ 
+    rp->rio_fd = fd; 
+    rp->rio_cnt = 0; 
+    rp->rio_bufptr = rp->rio_buf; 
+} 
+static ssize_t rio_read(rio_t *rp,char *usrbuf,size_t n) 
+{ 
+    int cnt; 
+    while(rp->rio_cnt <= 0){/*Read the file content if buf is empty*/ 
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,sizeof(rp->rio_buf)); 
+        if(rp->rio_cnt < 0){ 
+            if(errno != EINTR){ 
+                return -1; 
+            } 
+        }else if(rp->rio_cnt == 0){/*EOF*/ 
+            return 0; 
+        }else {/*reset buf ptr*/ 
+            rp->rio_bufptr = rp->rio_buf; 
+        } 
+    } 
+    /*when n < rp->rio_cnt, need copy some times */ 
+    cnt = n; 
+    if(rp->rio_cnt < n){/*one time copy end*/ 
+        cnt = rp->rio_cnt; 
+    } 
+    memcpy(usrbuf,rp->rio_bufptr,cnt); 
+    rp->rio_bufptr += cnt; 
+    rp->rio_cnt -= cnt; 
+    return cnt; 
+} 
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf,size_t maxlen) 
+{ 
+    int n,rc; 
+    char c,*bufp = usrbuf; 
+    for(n = 1; n < maxlen; n++){ 
+        if (( rc = rio_read(rp,&c,1)) == 1){ 
+            *bufp++ = c; 
+            if(c == '\n'){ 
+                break; 
+            } 
+        }else if (rc == 0){ 
+            if(n == 1){/*EOF no data read*/ 
+                return 0; 
+            }else{/*EOF some data read*/ 
+                break; 
+            } 
+        }else{/*ERROR*/ 
+            return -1; 
+        } 
+    } 
+    *bufp = 0;/*string end sign :'\0'*/ 
+    return n; 
+} 
+ 
+ssize_t rio_readnb(rio_t *rp,void *usrbuf,size_t n) 
+{ 
+    size_t nleft = n; 
+    ssize_t nread; 
+    char *bufp = usrbuf; 
+ 
+    while(nleft > 0){ 
+        if((nread = rio_read(rp,bufp, nleft)) < 0){ 
+            if(errno == EINTR){/*interrupted by sig handler return*/ 
+                nread =0; 
+            }else{/*errno set by read() */ 
+                return -1; 
+            } 
+        }else if(nread == 0){/*EOF*/ 
+            break; 
+        } 
+        nleft -= nread; 
+        bufp += nread; 
+    } 
+    return (n-nleft);/*return >=0*/ 
+} 
+ 
+int main() 
+{ 
+    int n; 
+    rio_t rio; 
+    char buf[MAXLINE]; 
+ 
+    int fd = open("1.txt",O_RDONLY,755); 
+    if(fd <=0){ 
+        printf("error\n"); 
+    } 
+    rio_readinitb(&rio,fd); 
+    while ((n = rio_readlineb(&rio,buf,MAXLINE)) != 0){ 
+           rio_writen(1,buf,n); 
+    } 
+    close(fd); 
+    return 0; 
+} 
 
-
-/*开始rio_readn 函数，fd文件描述符，n读取的字符数量，从文件描述种最多传送n个字符到存储器：*/
-ssize_t rio_readn(int fd,void *usrbuf,size_t n){
-	size_t  nleft = n;
-	ssize_t nread;
-	char *bufp = usrbuf;
-	while(nleft >0){
-		if((nread = read(fd,bufp,nleft)) < 0){
-			if(errno = EINTR){ //errno是全局变量，用于处理错误问题，linux错误问题的两种处理方式，errno和错误返回值
-				nread = 0;//被中断以后的处理
-			}
-			else 	
-				return -1; //read的最初设定值
-		}	
-		else if (nread == 0 ){
-			break;//eof 	
-		}
-		nleft -= nread;
-		bufp  += nread;
-	}
-	return (n-nleft);	//返回不足值或者期望值
-}/*结束rio_readn*/
-ssize_t rio_writen(int fd,void *usrbuf,size_t n){
-	size_t nleft = n;
-	ssize_t nwrite;
-	char *bufp = usrbuf;
-	while(nleft >0){
-		if(nwrite = write(fd,bufp,nleft))<=0){
-			if(errno =  EINTR){
-				nwrite = 0;//重新启动write流程
-			}
-			else 
-				return -1;写失败
-	}
-			nleft -= nwrite;
-			bufp  += nwrite;
-		
-	}
-	return n;//返回写入的内容，因为不存在eof的情况，所以必须返回设定值
-}
-#endif /*结束rio.h*/
